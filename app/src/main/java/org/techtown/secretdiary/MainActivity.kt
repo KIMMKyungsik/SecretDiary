5390aea279389f2be64bbe039224124c1b8fdd13
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-02-09 05:42:22 +0900

    AlertDialog를 이용한 비밀번호 확인 기능

--- app/src/main/java/org/techtown/secretdiary/MainActivity.kt
+++ app/src/main/java/org/techtown/secretdiary/MainActivity.kt
@@ -1,11 +1,88 @@
 package org.techtown.secretdiary
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply {
+                minValue = 0
+                maxValue = 9
+
+            }
+
+    }
+
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply {
+                minValue = 0
+                maxValue = 9
+
+            }
+
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply {
+                minValue = 0
+                maxValue = 9
+
+            }
+
+    }
+
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+
+    }
+
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+        openButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+
+                // TODO 다이어리 페이지 작성후에 넘겨주어야 함
+
+
+            } // 패스워드 성공
+            else {
+                AlertDialog.Builder(this)
+                    .setTitle("실패!!")
+                    .setMessage("비밀번호가 잘못되었습니다.")
+                    .setPositiveButton("확인") { _, _ -> }
+                    .create()
+                    .show()
+                //실패
+
+            }
+
+        }
+
     }
 }
